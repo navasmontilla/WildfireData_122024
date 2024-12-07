8de377c649c771bb4eab16aeee27c082a25006d4
Adrian <45123014+navasmontilla@users.noreply.github.com>
2024-12-07 22:27:02 +0100

    added files

---
+++ 2D_cases/WFM_model_2D_case3/firePlotting.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## WFM results
+# 
+
+# Libraries are imported and program folders and files are defined:
+
+# In[121]:
+
+
+import math                    
+import numpy as np             
+import matplotlib.pyplot as plt 
+from sklearn.linear_model import LinearRegression
+
+folder_base="output-files/"
+folder_sol=""
+folder_out=folder_base
+
+type_model=1 #1: (T,Y), 2: (H,Y)
+
+
+# Data is read:
+
+# In[122]:
+
+
+from glob import glob
+import os
+import re
+
+files = sorted(glob(folder_out + "/listFire0*.out"), key=lambda x: int(re.findall(r'\d+', os.path.basename(x))[0]))
+#files = glob(folder_out+"/listFire0*")
+lf=len(files)
+print("The number of files is", lf)
+
+#print(files)
+
+file1 = open(files[0], 'r')
+line = file1.readline()
+line = file1.readline()
+cells=re.findall(r'\b\d+\b',line)
+xcells=int(cells[0])
+ycells=int(cells[1])
+
+print("Mesh dimensions are:",xcells,"x",ycells)
+print("\nList of files read:\n")
+
+xc=np.zeros((xcells,ycells))
+yc=np.zeros((xcells,ycells))
+T=np.zeros((xcells,ycells,lf))
+Y=np.zeros((xcells,ycells,lf))
+H=np.zeros((xcells,ycells,lf))
+Rf=np.zeros((xcells,ycells,lf))
+
+
+j=0
+for fname in files:
+    print(fname+" file read")
+    data     = np.loadtxt(fname, skiprows=2)
+    k=0
+    for l in range(0,xcells):   
+        for m in range(0,ycells):
+            #k = l + m*xcells + n*xcells*ycells
+            xc[l,m]=data[k,0]
+            yc[l,m]=data[k,1]
+            T[l,m,j]=data[k,2]
+            Y[l,m,j]=data[k,3]
+            Rf[l,m,j]=data[k,4]
+            if type_model==2:
+                H[l,m,j]=data[k,5]
+            k+=1       
+    j=j+1   
+    
+times  = np.loadtxt(folder_out+"/times_fire.out")
+
+
+# **2D PLOT:**
+# 
+# This creates a 2D plot, to select the time use `j=...`, the index for the file that takes values from `0` to `lf-1`:
+
+# In[123]:
+
+
+j=-1 #file number (time)
+
+print("Plot at time:",times[j], "s")
+
+#filename = 
+
+xp = xc[:,0]     #puntos en x
+yp = yc[0,:]      #puntos en y
+Xp, Yp = np.meshgrid(xp, yp)    #matriz de puntos
+ST=np.transpose(T[:,:,j])
+SY=np.transpose(Y[:,:,j])
+SRf=np.transpose(Rf[:,:,j])
+  
+
+fig, ax = plt.subplots(1,2,figsize=(9,4.5))      #genera el objeto "figura"
+
+levels = np.linspace(np.min(T), np.max(T), 12)
+#print(levels)
+plot1=ax[0].contour(Xp, Yp, ST, 6,levels=levels,colors="k",linewidths=0.75)  
+plot1=ax[0].contourf(Xp, Yp, ST, 200, cmap='YlOrRd')   
+ax[0].set_title('T(K)')
+ax[0].set_xlabel("x(m)") 
+ax[0].set_ylabel("y(m)") 
+ax[0].set_aspect('equal', 'box')
+ax[0].plot([100,100],[1,199],'k--')
+ax[0].plot([1,199],[100,100],'k--')
+#plot1.set_clim( np.min(T), np.max(T) )
+plot1.set_clim( 300.0, 1400.0 )
+
+plot2=ax[1].contourf(Xp, Yp, SY, 200, cmap='Greens' )
+plot1=ax[1].contour(Xp, Yp, SY, 6,colors="k",linewidths=0.75) 
+ax[1].plot([100,100],[1,199],'k--')
+ax[1].plot([1,199],[100,100],'k--')
+
+
+ax[1].set_title('Y')
+ax[1].set_xlabel("x(m)") 
+ax[1].set_ylabel("y(m)") 
+ax[1].set_aspect('equal', 'box')
+
+#plot2=ax[2].contourf(Xp, Yp, SRf, 200, cmap='viridis' )
+#plot1=ax[2].contour(Xp, Yp, SRf, 12,colors="k",linewidths=0.75) 
+#ax[2].set_title('Rf(x,y)')
+#ax[2].set_xlabel("x") 
+#ax[2].set_ylabel("y") 
+#ax[2].set_aspect('equal', 'box')
+
+dx=xc[1,0]-xc[0,0]
+burned_area = dx*dx*np.sum(SY < 0.99)
+
+print("Dx is: ",dx)
+print("Burned area: ",burned_area)
+
+print("Max T is: ",np.max(ST))
+print("Min T is: ",np.min(ST))
+
+print("Max Y is: ",np.max(SY))
+print("Min Y is: ",np.min(SY))
+
+print("Max Rf is: ",np.max(SRf))
+print("Min Rf is: ",np.min(SRf))
+
+fig.text(0.09, 0.89, "M=0.0", fontsize=10)
+fig.text(0.43, 0.89, "M=0.1", fontsize=10)
+fig.text(0.09, 0.15, "M=0.2", fontsize=10)
+fig.text(0.43, 0.15, "M=0.3", fontsize=10)
+
+
+
+
+SRf=np.transpose(Rf[:,:,0])
+for j in range(1,lf):
+    SY=np.transpose(Y[:,:,j])
+    plot3=ax[1].contour(Xp, Yp, SY, [0.99],colors="r",linewidths=0.75) 
+
+fig.tight_layout()
+
+fig.savefig("fig_2D_TY.png",dpi=500)  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+# **1D PLOT (x direction):**
+# 
+# This creates a 1D plot at different times. Use `it` to select the number of series:
+
+# In[125]:
+
+
+nfiles=lf
+fig, ax  = plt.subplots(2,1,figsize=(10, 5))
+it=4
+for i in range(0,nfiles,it):
+    ax[0].plot(xc[:,m],T[:,m,i],'o-',ms=5,label='Time '+str(float(np.round(times[i],1)))+' s')
+    
+ax[0].set_xlabel("x(m)") 
+ax[0].set_ylabel("T(K)") 
+#ax[0].set_ylim([200,np.max(T)])
+ax[0].legend()
+
+
+for i in range(0,nfiles,it):
+    ax[1].plot(xc[:,m],Y[:,m,i],'o-',ms=5,label=str((times[i],2)))
+ax[1].set_xlabel("x(m)") 
+ax[1].set_ylabel("Y") 
+ax[1].set_ylim([0,1])
+
+
+
+fig.tight_layout()
+
+image_path = "fig_solution.png"
+fig.savefig(image_path,dpi=400)
+
+# **Calculation of the Rate of Spread (ROS)**
+
+# In[126]:
+
+
+frontLoc=np.zeros(nfiles)
+vel=np.zeros(nfiles)
+
+Tinf=T[-1,m,0]
+
+for j in range(nfiles):
+    Tmax=np.max(T[int(xcells/2):-1,m,j])
+    Tmid=0.5*(Tmax+Tinf)
+    pospeak=np.argmax(T[int(xcells/2):-1,m,j])+int(xcells/2)
+    Tdif=np.abs(T-Tmid)
+    posmax=np.argmin(Tdif[pospeak:-1,m,j])+pospeak    
+    frontLoc[j]=xc[posmax,m]
+    if j>0:
+        vel[j]=(frontLoc[j]-frontLoc[j-1])/(times[j]-times[j-1])
+
+frontLoc[0]=xc[int(xcells/2),m]        
+
+j=-1
+print('The position of the front at the final time',float(times[j]),'s is', frontLoc[j],'m')
+
+fig, ax  = plt.subplots(figsize=(5, 4))
+ax2 = ax.twinx()
+ax2.plot(xc[posmax-80:posmax+20,m],Y[posmax-80:posmax+20,m,j],'o-',color="tab:orange",ms=5,label="Y")
+ax2.set_xlabel("x (m)") 
+ax2.set_ylabel("Y")
+ax2.set_ylim([-0.07,1.07])
+ax.plot(xc[posmax-80:posmax+20,m],T[posmax-80:posmax+20,m,j],'o-',ms=5,label="T")
+ax.plot(frontLoc[j],Tmid,'o')
+ax.set_xlabel("x (m)") 
+ax.set_ylabel("T (K)") 
+ax.set_title("Solution at final time "+str(float(times[j]))+' s') 
+
+image_path = "fig_prfile.png"
+fig.savefig(image_path,dpi=400)
+
+
+fig, (ax,ax2)  = plt.subplots(1,2,figsize=(10, 4))
+ax.plot(times,frontLoc,'o-')
+ax.set_xlabel("time (s)") 
+ax.set_ylabel("$x_{front}$ (m)") 
+ax.set_title("Position of the front vs time" )
+ax2.plot(times,vel,'-')
+ax2.set_xlabel("time (s)") 
+ax2.set_ylabel("ROS (m/s)") 
+ax2.set_title("Instantaneous ROS" )
+
+image_path = "fig_ROS.png"
+fig.savefig(image_path,dpi=400)
+
+times = times.reshape(-1,1)
+frontLoc = frontLoc.reshape(-1,1)
+regression_model = LinearRegression()
+regression_model.fit(times, frontLoc)
+print('The ROS is:' ,float(regression_model.coef_), 'm/s')
+
+
+
+
+
+
+
---
+++ 2D_cases/WFM_model_2D_case5/fireSetup.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## WFM setup
+# 
+
+# In[121]:
+
+
+import math                    
+import numpy as np             
+import matplotlib.pyplot as plt 
+
+folder_case="case"
+folder_out="output-files"
+
+fname_config="configure.input"
+fname_configF="configureFire.input"
+fname_initialF="initialFire.out"
+fname_landscapeF="landscapeFire.out"
+
+
+# Global configuration
+
+# In[122]:
+
+
+#Simulation setup - Global settings
+FinalTime = 2600.0  #this is the final time before 800
+SizeX = 200.0      #this is the domain size in X
+SizeY = 200.0      #this is the domain size in Y
+SizeZ = 100.0
+
+#Fire model setup
+DumpTime = 200.0    #time period to write output files
+CFL = 0.4          #Courant-Friedrichs-Lewy stability criterion. Must be <0.5
+Order = 7          #Order of accuracy for advection. Should be 5 or 7.
+
+#Mesh setup
+xcells = 400       #number of cells in X
+ycells = 400       #number of cells in Y
+
+#Boundary conditions (do not modify them!)
+Face_1 = 3 #-y
+Face_2 = 3 #+x
+Face_3 = 3 #+y
+Face_4 = 3 #-x
+
+#Other parameters (constant in the domain)
+u_x = 0.000000000  #m/s
+u_y = 0.500  #m/s
+K   = 0.00010   #kW/(m·K)
+h   = 0.05   #kW/(m^3·K)
+Tinf= 300.0 #K
+RH  = 0.30   #aodim (relative humidity)
+L   = 1.0   #m
+
+
+# No need to modify the following cell
+
+# In[123]:
+
+
+dx=SizeX/xcells
+dy=SizeY/ycells
+
+T=np.zeros((xcells,ycells))
+Y=np.zeros((xcells,ycells))
+rhof=np.zeros((xcells,ycells))
+Cf=np.zeros((xcells,ycells))
+H=np.zeros((xcells,ycells))
+Z=np.zeros((xcells,ycells))
+alpha=np.zeros((xcells,ycells))
+Tpc=np.zeros((xcells,ycells))
+SC=np.zeros((xcells,ycells))
+Rfmax=np.zeros((xcells,ycells))
+Mg=np.zeros((xcells,ycells))
+A=np.zeros((xcells,ycells))
+    
+x=np.arange(0+dx/2.0, SizeX, dx)
+y=np.arange(0+dy/2.0, SizeY, dy)
+
+xc, yc= np.meshgrid(x,y,indexing='ij')
+
+
+# The **initial condition** and other properties are set here:
+
+# In[124]:
+
+
+xcenter=100
+ycenter=50
+
+#example of a circular initial fire with radius r=15 and random initial biomass with maximum Y=0.75
+for l in range(0,xcells):   
+    for m in range(0,ycells):
+        #r=np.sqrt((xc[l,m]-xcenter)**2+(yc[l,m]-ycenter)**2)  
+        r=np.sqrt((xc[l,m]-xcenter)**2+(yc[l,m]-ycenter)**2)
+        if r<5:
+            T[l,m]=670.0
+        else:
+            T[l,m]=300.0
+        
+        Y[l,m]=1.0
+        
+#landscape properties         
+
+        Rfmax[l,m]=0.0100   #maximum volumetric porosity
+        SC[l,m]=0.25*( np.sin(xc[l,m]/20.0)*np.cos(yc[l,m]/10.0-20.0) + np.sin(xc[l,m]/20.0-40.0)**2*np.sin(yc[l,m]/10.0)+2.0 ) #surface coverage
+        Z[l,m]=0.0#50.0*np.exp(-((xc[l,m]-150)**2+(yc[l,m]-100)**2)/4000) #topography
+        rhof[l,m]=400.0 #solid fuel density
+        Cf[l,m]=1.00 #solid fuel specific heat
+        H[l,m]=4000 #solid fuel heat power
+        Mg[l,m]=0.0000 #green wood water content
+        alpha[l,m]=1.0 #ratio between green and season wood
+        Tpc[l,m]=600.0 #pirolisis temperature
+        A[l,m]=0.01 #reaction rate
+
+
+
+f = open(folder_case+"/"+fname_config, "w")
+f.write("/////SIMULATION_SETUP////// \n")
+f.write("FinalTime    "+str(FinalTime)+"\n")
+f.write("SizeX    "+str(SizeX)+"\n")
+f.write("SizeY    "+str(SizeY)+"\n")
+f.write("SizeZ    "+str(SizeZ)+"\n")
+f.close()
+
+f = open(folder_case+"/"+fname_configF, "w")
+f.write("/////SIMULATION_SETUP////// \n")
+f.write("DumpTime    "+str(DumpTime)+"\n")
+f.write("CFL    "+str(CFL)+"\n")
+f.write("Order    "+str(Order)+"\n")
+f.write("////////MESH_SETUP/////////\n")
+f.write("xcells    "+str(xcells)+"\n")
+f.write("ycells    "+str(ycells)+"\n")
+f.write("///////BOUNDARY_COND///////\n")
+f.write("Face_1    "+str(Face_1)+"\n")
+f.write("Face_2    "+str(Face_2)+"\n")
+f.write("Face_3    "+str(Face_3)+"\n")
+f.write("Face_4    "+str(Face_4)+"\n")
+f.write("/////////PARAMETERS////////\n")
+f.write("u_x(m/s)    "+str(u_x)+"\n")
+f.write("u_y(m/s)    "+str(u_y)+"\n")
+f.write("rho(kg/m^3)    40.0 \n") #40
+f.write("C(kJ/(kg·K))   1.0 \n") #1.0
+f.write("H(kJ/kgFuel)   4000.0\n") #4000
+f.write("K(kW/(m·K))    "+str(K)+"\n")
+f.write("h(kW/(m^3·K))    "+str(h)+"\n")
+f.write("Tinf(K)    "+str(Tinf)+"\n") 
+f.write("Tpc(K)    400\n") #400
+f.write("RH    "+str(RH)+"\n") 
+f.write("L(m)    "+str(L)+"\n")
+f.close()
+
+f = open(folder_case+"/"+fname_initialF, "w")
+f.write("VARIABLES = X, Y, T, Y \n")
+f.write("CELLS = "+str(xcells)+", "+str(ycells)+"\n")
+for l in range(0,xcells):   
+    for m in range(0,ycells):
+        f.write(str(xc[l,m])+" "+str(yc[l,m])+" "+str(T[l,m])+" "+str(Y[l,m])+"\n")
+f.close()  
+
+
+f = open(folder_case+"/"+fname_landscapeF, "w")
+f.write("VARIABLES = X, Y, Z, Rfmax, SC, rhof, Cf, Mg, alpha, Tp, H, A \n")
+f.write("CELLS = "+str(xcells)+", "+str(ycells)+"\n")
+for l in range(0,xcells):   
+    for m in range(0,ycells):
+        f.write(str(xc[l,m])+" "+str(yc[l,m])+" "+str(Z[l,m])+" "+str(Rfmax[l,m])+" "+str(SC[l,m])+" "+str(rhof[l,m])+" "+str(Cf[l,m])+" "+str(Mg[l,m])+" "+str(alpha[l,m])+" "+str(Tpc[l,m])+" "+str(H[l,m])+" "+str(A[l,m])+"\n")
+f.close()  
+
+print("Files written")
+
+
+# In[ ]:
+
+
+
+
